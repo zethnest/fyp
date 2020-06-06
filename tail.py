f6db017a79b8aa0c58e0ce9c3d57c3a503b2227d
zethnest <16971676+zethnest@users.noreply.github.com>
2020-06-06 11:16:01 +0800

    add Time class
    cleanup

--- tail.py
+++ tail.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import math
 import time
 import re
 
@@ -13,7 +14,26 @@ class color:
     end = '\033[0m'
     bold = '\033[1m'
     underline = '\033[4m'
-colors = [color.green , color.blue , color.yellow]
+altColors = [color.green , color.blue , color.yellow]
+
+class Time:
+    def __init__(self, strHour, strMinute, strSecond, strMillisecond):
+        self.__hour = int(strHour)
+        self.__minute = int(strMinute)
+        self.__second = int(strSecond)
+        self.__millisecond = float(f"0.{strMillisecond}")
+
+    def diffTime(self, otherTime):
+        return abs(self.toSeconds() - otherTime.toSeconds())
+
+    def toSeconds(self):
+        return self.__hour*3600 + self.__minute*60 + self.__second + self.__millisecond
+
+    def fromSeconds(seconds):
+        return Time(math.floor(seconds/3600%60), math.floor(seconds/60%60), math.floor(seconds%60), float(seconds-int(seconds)))
+
+def altColor(num):
+    return altColors[num%len(altColors)]
 
 bufferLog = ''
 printCount = 0
@@ -37,23 +57,53 @@ ddosWatchlist = {
         "byMAC": {},
         "byIP": {},
         }
+lastTime = {}
+firstPacket = {}
+packetInLimit = {}
+packetLimit = 15
+
 def icmpHandler(log):
+    global lastTime
+    global firstPacket
     timeMatch = re.match(r"(\d+):(\d+):(\d+)\.(\d+)", log)
     addressMatch = re.search(r"(\S+)\s>\s([^:]+):", log)
     hour = timeMatch.group(1)
     minute = timeMatch.group(2)
     second = timeMatch.group(3)
-    milisecond = timeMatch.group(4)
+    millisecond = timeMatch.group(4)
     source = addressMatch.group(1)
     destination = addressMatch.group(2)
+
+    currentTime = Time(hour, minute, second, millisecond)
+
+    diffSecond = 0
+    if source in lastTime:
+        diffSecond = currentTime.diffTime(lastTime[source])
+
+        if source in firstPacket:
+            if diffSecond > 3:
+                firstPacket[source] = currentTime
+            else:
+                if source in packetInLimit:
+                    packetInLimit[source] += 1
+        else:
+            firstPacket[source] = currentTime
+
     if source in ddosWhitelist["source"] or destination in ddosWhitelist["destination"]:
         oldPrint(f"Skipped {source} & {destination}")
-        print(f"{colors[printCount%len(colors)]}{log}{color.end}")
+        print(f"{altColor(printCount)}{log}{color.end}")
         return
     if source in ddosBlacklist or destination in ddosBlacklist:
         #TODO implement blocking
         oldPrint("block port")
-    print(f"{colors[printCount%len(colors)]}{hour}:{minute}:{second} | {source} > {destination}{color.end}")
+        return
+
+    print(f"{altColor(printCount)}"+
+          f"{diffSecond:.5f}s | {source} > {destination}"+
+          f"{color.end}")
+    #print(f"{altColor(printCount)}{log}{color.end}")
+
+    lastTime[source] = currentTime
 
 def parse(line):
     global bufferLog, printCount
@@ -65,7 +115,6 @@ def parse(line):
         if re.search("proto ICMP", bufferLog):
             icmpHandler(bufferLog)
 
-        print(f"{colors[printCount%len(colors)]}{bufferLog}{color.end}")
         bufferLog = line
     else:
         bufferLog += line
